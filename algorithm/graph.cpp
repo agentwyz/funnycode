201b816accc0a5a0b09ba98f48f2e3f6e47de00c
agentwyz <2012663060@qq.com>
2023-02-11 20:48:39 +0800

    add some code

---
+++ algorithm/graph.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+//dfs
+void dfs()
+{
+	for () {
+		
+	}
+} 
+
+//spfa
+void add(int a, int b, int c) 
+{	
+	w[idx] = c;
+	e[idx] = b, ne[idx] = h[a], h[a] = idx++;		
+}
+
+int dji()
+{
+	for () {
+		for () {
+			
+		}
+		
+		for () {
+			
+		}
+	}
+}
+
+int bf()
+{
+	for () {
+		//备份数组 
+		for () {
+			
+		}
+	}
+}
+
+int spfa()
+{
+	queue<int> q;
+	while (q) {
+		
+	}
+}
+
+int main() {
+	
+}
