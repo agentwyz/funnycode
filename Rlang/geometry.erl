f77d79a1aab69f9d95f069df978bf4ccf49dbb49
agentwyz <2012663060@qq.com>
2023-06-11 15:03:53 +0800

    Added the files

--- Rlang/geometry.erl
+++ Rlang/geometry.erl
@@ -1,11 +1,17 @@
 -module(geometry).       %%这是一行模块声明
 -export([test/0, area/1]).
 
+%%关于符号的使用
+%%逗号分隔函数 数据构造 模式中的参数调用,
+%%分号分隔子句;
+%%句号(.) 分隔函数
+
 
 %%执行函数genmetry:area({rectangle, 10, 5})
 %%执行函数genmetry:area({square, 10})
 area({rectangle, Width, Heigit}) -> Width * Heigit;
-area({square, Side})             -> Side * Side.
+area({square, Side})             -> Side * Side;
+area({circle, Radius})           -> 3.1459 * Radius * Radius.
 
 
 test() ->
