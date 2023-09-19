5ac8697557aab9ea0e7282b38194f8639374aa64
agentwyz <2012663060@qq.com>
2023-09-20 01:10:24 +0800

    add the lazy.rkt

--- code/racket/lazy.rkt
+++ code/racket/lazy.rkt
@@ -10,6 +10,8 @@
             ((assq x env) => cdr)
             (else #f))))
 
+;;每次造一个thunk
+;;每一个函数可能被缓存
 (struct thunk (fv cached?) #:transparent #:mutable)
 
 (struct closure (fun env))
@@ -17,3 +19,19 @@
 (define make-thunk
     (lambda (fun)
         (thunk fun #f)))
+
+;;接收一个严格计算的函数
+;;使用cond语句来检查thunk是否被缓存
+;;如果thunk被缓存
+(define force-thunk
+    (lambda (th)
+        (cond 
+            [(thunk-cached? th) (thunk-fv th)]
+            [else 
+                (let loop ([val ((thunk-fv th))])
+                    (cond 
+                        [(thunk? val) (loop ((thunk-fv val)))]
+                        [else 
+                            (set-thunk-fv! th val)
+                            (set-thunk-cached?! th #t) 
+                            val]))])))
