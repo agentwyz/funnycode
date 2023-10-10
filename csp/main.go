d2321a2955f27ba8b38da25dfda6d7a554febfe0
agentwyz <2012663060@qq.com>
2023-10-10 22:53:51 +0800

    fix some bug

--- csp/main.go
+++ csp/main.go
@@ -1,31 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
 
-func coordinateWaitGroup() {
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	num := int32(0)
-	fmt.Printf("The number: %d [with sync.WaitGroup]\n", num);
-
-	max := int32(10)
-
-	go addNum(&num, 1, max, func(){
-		//其它阻塞
-		sign<-struct{}{}
-	})
-
-	go addNum(&num, 2, max, func(){
-		sign<-struct{}{}
-	})
-
-	<-sign
-	<-sign
-}
 
 func main() {
 
